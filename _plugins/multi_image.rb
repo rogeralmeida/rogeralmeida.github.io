f8ac1db7f54549ac9a14b0489ba283b9b66da8c0
roger.eduardo@gmail.com <roger.eduardo@gmail.com>
2018-03-24 18:40:30 +1100

    Plugin to generate responsive images

--- _plugins/multi_image.rb
+++ _plugins/multi_image.rb
@@ -1,17 +1,23 @@
 require "mini_magick"
+require 'logger'
 require 'fileutils'
 
-module MultiImage 
-  class Generator < Jekyll::Generator
-    def generate(site)
-      FileUtils.mkdir_p("#{Dir.pwd}/_site/images/thumbnails") unless Dir.exist?("#{Dir.pwd}/_site/images/thumbnails")
-      site.static_files.each do |file|
-        if ['.png', '.jpg', '.jpeg'].include?(file.extname)
-          file_name = File.basename file.path, file.extname
-          image = MiniMagick::Image.open(file.path)
-          image.resize '100x100'
-          image.format 'png'
-          image.write "#{Dir.pwd}/_site/images/thumbnails/#{file_name}.png"
+Jekyll::Hooks.register :site, :post_write do |site|
+
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::INFO
+  i_sizes = [1024, 768, 640, 480, 240]
+  site.static_files.each do |file|
+    if ['.png'].include?(file.extname) && file.path.include?("images/responsive")
+      file_name = File.basename file.path, file.extname
+      image = MiniMagick::Image.open(file.path)
+      i_sizes.each do |size|
+        new_file = "#{Dir.pwd}/_site/images/responsive/#{file_name}-#{size}.png"
+        if image.width > size && !File.file?(new_file)
+          image.resize "#{size}x"
+          logger.info "Saving new image: #{new_file}"
+          image.write new_file
+          raise IOError, "new image was not saved" unless File.file?(new_file)
         end
       end
     end
