8afab5022b2f3c8edab8e1516d58e70480894de0
Roger Almeida <ralmeida@tyro.com>
2019-03-24 22:19:14 +1100

    Disabling my custom plugins

--- _plugins/api_plugin.rb
+++ _plugins/api_plugin.rb
@@ -7,27 +7,29 @@ Jekyll::Hooks.register :site, :post_write do |site|
     logger.level = Logger::INFO
     logger.info site.print_stats
 
-    post_list = []
-    site.posts.docs.each do |post|
-        logger.info "Data: #{post.data}"
-        json_file = {
-            title: post.data["title"],
-            excerpt: post.data["excerpt"],
-            url: post.url
-        }
-        new_file = "#{Dir.pwd}/_site/api#{post.url}.json"
-        dirname = File.dirname(new_file)
-        unless File.directory?(dirname)
-            FileUtils.mkdir_p(dirname)
-        end
-        File.open(new_file, "w") do |file|
-            logger.info "Creating file: #{file.path}"
-            file.write(json_file.to_json)
-        end
-        post_list << json_file
-    end
-    File.open("#{Dir.pwd}/_site/api/all.json", "w") do |file|
-        logger.info "Creating all file: #{file.path}"
-        file.write(post_list.to_json)
+    if false
+      post_list = []
+      site.posts.docs.each do |post|
+          logger.info "Data: #{post.data}"
+          json_file = {
+              title: post.data["title"],
+              excerpt: post.data["excerpt"],
+              url: post.url
+          }
+          new_file = "#{Dir.pwd}/_site/api#{post.url}.json"
+          dirname = File.dirname(new_file)
+          unless File.directory?(dirname)
+              FileUtils.mkdir_p(dirname)
+          end
+          File.open(new_file, "w") do |file|
+              logger.info "Creating file: #{file.path}"
+              file.write(json_file.to_json)
+          end
+          post_list << json_file
+      end
+      File.open("#{Dir.pwd}/_site/api/all.json", "w") do |file|
+          logger.info "Creating all file: #{file.path}"
+          file.write(post_list.to_json)
+      end
     end
 end
